74023bda56417a3d68b7e1b8e32783cb6c0a4dee
Valery Likhovskikh <java2ee@list.ru>
2019-03-15 08:50:56 +0300

    Update TypeData.java

--- ros/src/ru/transset/esma/TypeData.java
+++ ros/src/ru/transset/esma/TypeData.java
@@ -37,6 +37,14 @@ public class TypeData extends Brick {
 		this.sql = sql;
 	}
 
+	/**
+	 * Добавляет поле типа данных. Этот метод вызывается при обработке
+	 * файла конфигурации.
+	 * 
+	 * @param name имя поля типа данных
+	 * @param element экземпляр поля типа данных
+	 * @return {@code true}, если поле типа данных успешно добавлен, иначе {@code false} 
+	 */
 	public boolean addElement(String name, Element element) {
 		// для установки значения поля требуется только имя элемента
 		elements.add(name);
