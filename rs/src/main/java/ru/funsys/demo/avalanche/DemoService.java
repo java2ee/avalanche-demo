f580f7907ab9c69027541f898fbc62bf0ec7cdd6
Валерий Лиховских <likhovskikh_vv@transset.ru>
2019-04-23 08:41:10 +0300

    Убрал не используемые импортируемые классы

--- rs/src/main/java/ru/funsys/demo/avalanche/DemoService.java
+++ rs/src/main/java/ru/funsys/demo/avalanche/DemoService.java
@@ -5,17 +5,11 @@ package ru.funsys.demo.avalanche;
 
 import java.util.Hashtable;
 
-import javax.inject.Singleton;
-import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -29,6 +23,11 @@ public class DemoService {
 	
 	private DemoApplication application;
 	
+	/*
+	 * Устанавливает ссылку на экземпляр класса Application
+	 * Этот метод вызывается при создании экземпляра класса сервиса при чнении конфигурационного
+	 * файла.
+	 */
 	public void setApplication(DemoApplication application) {
 		this.application = application;
 	}
